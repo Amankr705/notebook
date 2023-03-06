63698caf2c5ef1a1a406848bc44703a6258b1e21
Amankr705 <ak.1000aman@gmail.com>
2023-03-07 01:24:05 +0530

    useLocation hook

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,21 +2,9 @@ import NoteContext from "./noteContext";
 import { useState } from "react";
 
 const NoteState = (props)=>{
-    const s1 = {
-        "name": "Aman",
-        "class": "XII"
-    }
-    const [state, setstate] = useState(s1);
-    const update = ()=>{
-        setTimeout(() => {
-            setstate({
-                "name": "Aman kumar",
-                "class": "XIII"
-            })
-        }, 1000);
-    }
+    
     return(
-        <NoteContext.Provider value={{state:state, update:update}}>
+        <NoteContext.Provider value={{}}>
             {props.children}
         </NoteContext.Provider>
     )
