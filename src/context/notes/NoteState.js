8710fb32a809d59e0c0b2df2331d69574e623e72
Amankr705 <ak.1000aman@gmail.com>
2023-03-07 13:52:27 +0530

    Added AddNote component to Notebook

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -49,12 +49,37 @@ const NoteState = (props)=>{
               "__v": 0
             }
     ]
-
     const [notes, setNotes] = useState(notesInitial)
-    
+
+    // Add a Note
+    const addNote = (title, description, tag)=>{
+      // TODO API Call
+      console.log("Adding a new  note")
+      const note = {
+        "_id": "6403a54754b5802238b6eeb6",
+        "user": "6403822635aa2389264dad39",
+        "title": title,
+        "description": description,
+        "tag": tag,
+        "date": "2023-03-04T20:08:39.954Z",
+        "__v": 0
+      };
+      setNotes(notes.concat(note))
+
+    }
+
+    // Delete a Note
+    const deleteNote = ()=>{
+
+    }
+
+    // Edit a Note
+    const editNote = ()=>{
+
+    }    
     
     return(
-        <NoteContext.Provider value={{notes, setNotes}}>
+        <NoteContext.Provider value={{notes, addNote, deleteNote, editNote}}>
             {props.children}
         </NoteContext.Provider>
     )
